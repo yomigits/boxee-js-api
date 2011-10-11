4c38bc760112c8733829e7a789f5fa5cf31e6ae1
Raymond Cohen <ray@boxee.tv>
2011-10-11 11:03:38 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+boxee-api.js
+---
+
+javascript library to 
+
+TO USE IT
+---
+You must include it on your page after including the jQuery and Underscore libaries
+
+
+API
+---
+
+The library will create a global variable called `boxeeAPI`.
+
+	boxeeAPI.keyboardMode(); // Set the boxee remote to keyboard mode.  The d-pad on the remote will map to the arrow keys.  The center button will map to the Enter key.
+
+	boxeeAPI.cursorMode();   // Set the boxee remote to cursor mode.  The d-pad will move around the mouse cursor on the screen.  The center button will map to the clicking the mouse
+
+
+more coming soon
+
---
+++ boxee-api.js
@@ -0,0 +1,121 @@
+
+var defineBoxeeControlFunctions = function () {
+  function setApiMinVersion(args) {
+    version = args[0];
+    boxee.apiMinVersion = version || 7.0;
+  }
+  boxee.exec(setApiMinVersion);
+
+  function showNotification(args) {
+    var message = args[0];
+    var thumbUrl = args[1] || '.';
+    var duration = args[2] || 2;
+    
+    boxee.showNotification(message, thumbUrl, duration);
+  }
+  boxee.exec(showNotification);
+
+  function cursorMode() {
+    boxee.setMode(boxee.CURSOR_MODE);
+  }
+  boxee.exec(cursorMode);
+
+  function keyboardMode() {
+    boxee.setMode(boxee.KEYBOARD_MODE);
+  }
+  boxee.exec(keyboardMode);
+  
+  function getHttp(args) {
+    url = args[0];
+    return boxee.getHttp(url);  
+  }
+  boxee.exec(getHttp);
+  
+  function openDialogCallback(confirmed) {
+    boxee.showNotification("in openDialogCallback " + boxee.browserCallback);
+    //browser.execute("window.hi('" + boxee.browserCallback + "(" + confirmed + ")')");
+    browser.execute("window." + boxee.browserCallback + "(" + confirmed + ")");
+  }
+  boxee.exec(openDialogCallback);
+  
+  function promptDialog(args) {
+    var title = args[0];
+    var moreText = args[1] || '';
+    
+    boxee.browserCallback = args[2];
+    
+    boxee.openDialog("YesNo", title, moreText, 'openDialogCallback');
+  }
+  boxee.exec(promptDialog);
+}
+
+if (!window.boxee) {
+  window.boxee = {
+    fake: true,
+    exec: function(string) {
+      console.log("boxee.exec : " + string);
+    },
+    exec2: function(string) {
+      console.log("boxee.exec2 : " + string);
+      return '';
+    }
+  }
+} else {
+  defineBoxeeControlFunctions();
+}
+
+window.boxeeAPI = {
+  
+  notify: function(message, seconds) {
+    seconds = seconds ? seconds : 2;    
+    var json = JSON.stringify([message, ".", seconds]);
+    boxee.exec('showNotification(' + json + ')');
+  },
+  showBoxeeOSD: function() {
+    boxee.exec("boxee.showBoxeeOSD()");
+  },
+  getURL: function(url, callback) {
+    return $.Deferred(function(dfd) {
+      var json = JSON.stringify([url]);
+      var data = boxee.exec2("getHttp(" + json + ")");
+      setTimeout(function() {
+        dfd.resolve();
+        callback(data);
+      }, 0);
+    }).promise();
+  },
+  clearPauseOverlay: function() {
+    boxee.exec("playerState.isPaused = false;");
+  },
+  showPauseOverlay: function() {
+    boxee.exec("playerState.isPaused = true;");
+  },
+  keyboardMode: function() {
+    boxee.exec("keyboardMode()");
+  },
+  cursorMode: function() {
+    boxee.exec("cursorMode()");
+  },
+  showNotification: function() {
+    var array = Array.prototype.slice.call(arguments);
+    var json = JSON.stringify(array);
+    boxee.exec("showNotification(" + json + ")");
+  },
+  promptDialog: function(title, moreText, callback) {
+    if (typeof callback === 'function') {
+      boxee.promptDialogCallback = function(confirmed) {
+        callback(confirmed);
+      }
+      var json = JSON.stringify([title, moreText, 'boxee.promptDialogCallback']);
+      boxee.exec("promptDialog(" + json + ")");
+    } else if (typeof callback === 'string') {
+      var array = Array.prototype.slice.call(arguments);
+      var json = JSON.stringify(array);
+      boxee.exec("promptDialog(" + json + ")");
+    }
+  }
+};
+
+$(window).unload(function() {
+  boxee.clearPauseOverlay()
+});
